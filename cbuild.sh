1fb083cb7ea3e4b5bce3f5223144d4403d705cf9
Nina Herrmann <ninaherrmann@D-3170L10.WIWI.UNI-MUENSTER.DE>
2024-11-16 14:29:45 +0100

    cbuild config

--- cbuild.sh
+++ cbuild.sh
@@ -19,8 +19,9 @@ mkdir Model
 mkdir Output
 # $(seq 5 5 40)
 
-for i in 20 30; do
-  for j in 3 5; do
+
+for i in 40 50 60; do
+  for j in 5 7; do
     echo "${i} ${j}"
     "../lightgbm" config=train.conf num_trees=$i max_depth=$j output_model=Model/model_trees${i}_depth${j}.txt > Output/train_trees${i}_depth${j}.output
   done
