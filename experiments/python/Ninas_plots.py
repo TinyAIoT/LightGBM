3c6cc3e09084056c4024845eb95d8a852b17bef4
Nina Herrmann <ninaherrmann@D-3170L10.WIWI.UNI-MUENSTER.DE>
2024-11-23 21:55:05 +0100

    some graph things

--- experiments/python/Ninas_plots.py
+++ experiments/python/Ninas_plots.py
@@ -8,6 +8,95 @@ import re
 import lightgbm as lgb
 from pandas import read_csv
 from sklearn.metrics import accuracy_score, roc_auc_score, mean_squared_error, r2_score
+# keyword is the substring of the filename to search for in model.txt and .out files
+def plotMetrics(df, log_scale=False, dataset=None, dont_plot=False, get_baseline=True):
+    setting_value = df['tinygbdt_penalty_split']
+    plt.ioff()
+    fig1, ax1 = plt.subplots()
+    color = 'tab:red'
+    ax1.set_xlabel('tinygbdt_penalty_split')
+    if log_scale:
+    # 	ax2.set_yscale('log')
+        ax1.set_xscale('log')
+    if len(df['logloss']) > 0:
+        if df['logloss'].iloc[-1] != 0:
+            ax1.set_ylabel('Logloss')
+            ax1.plot(setting_value, df['logloss'], color=color, label='Logloss')
+    if len(df['rmse']) > 0:
+        if df['rmse'].iloc[-1] != 0:
+            ax1.set_ylabel('R2 Score')
+            ax1.plot(setting_value, df['accuracy'], color=color, label='R2 Score')
+    ax1.plot(setting_value, df['accuracy'], color='tab:purple', label='Accuracy/R2')
+    plt.legend(loc='lower center')
+    ax1.tick_params(axis='y', labelcolor=color)
+    ax2 = ax1.twinx()  # instantiate a second Axes that shares the same x-axis
+    ax2.set_ylabel('count')
+    ax2.plot(setting_value, df['no_thresholds'], label="no. thresholds")
+    ax2.plot(setting_value, df['no_leaves'], label="no. leaves")
+    plt.legend(loc='lower left')
+
+
+    ax4 = ax1.twinx()  # instantiate a second Axes that shares the same x-axis
+    ax4.yaxis.set_label_position("left")
+    ax4.yaxis.tick_left()
+    ax4.set_ylabel('bit count')
+    ax4.plot(setting_value, df['our_bits'], color='tab:pink', label="no. bits")
+    ax4.plot(setting_value, df['lgb_bits'], color='tab:brown', label="no. bits LGBM")
+    plt.legend(loc='lower right')
+
+
+    color = 'tab:green'
+    ax3 = ax1.twinx()  # instantiate a third Axes that shares the same x-axis
+    ax3.tick_params(axis='y', labelcolor=color)
+    ax3.plot(setting_value, df['no_features'], label="no. features", color=color)
+    ax3.plot(setting_value, df['no_trees'], label="no. trees", color='tab:cyan')
+
+    fig1.tight_layout()  # otherwise the right y-label is slightly clipped
+    plt.legend(loc='upper left')
+
+    plt.savefig(
+        '../plots/'
+        +'_penF'+str(df['tinygbdt_penalty_feature'].iloc[-1])
+        +'_penT'+str(df['tinygbdt_penalty_split'].iloc[-1])
+        +'_maxdepth'+str(df['depth'].iloc[-1])
+        +'_logscale'+str(log_scale)
+        +'.png'
+        )
+
+    plt.show()
+
+def plot_grid(df, title='Penalty Grid Search'):
+    plt.scatter(df['tinygbdt_penalty_split'], df['tinygbdt_penalty_feature'], c=df['accuracy'], cmap='viridis',
+                label='Accuracy')
+
+    max_accuracy = df['accuracy'][1:].max()
+    print(max_accuracy)
+
+    # Find rows where 'accuracy' matches max_accuracy
+    df_max_accuracy = df.loc[df['accuracy'] == max_accuracy]
+
+    # Assuming there's a single row with max accuracy, extract the first element
+    split_penalty = df_max_accuracy['tinygbdt_penalty_split'].iloc[0]
+    feature_penalty = df_max_accuracy['tinygbdt_penalty_feature'].iloc[0]
+
+    # Scatter and annotate the point with the max accuracy
+    pcm = plt.scatter(split_penalty, feature_penalty, c='r', label='Max Accuracy')
+    plt.annotate('Max Accuracy', (split_penalty, feature_penalty))
+
+    print("Dataframe max accuracy")
+    summary = df.describe()
+    print(summary)
+
+    plt.title(title)
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.xlabel('Split Penalty')
+    plt.ylabel('Feature Penalty')
+    plt.colorbar()
+    plt.legend()
+    plt.show()
+    plt.savefig('../plots/' + 'penalty_grid_.png')
+    # plt.tight_layout()
 
 def plotAccuracyByPenalty(df, keyword, plot_accuracy=True, plot_nodeLeafCount=False, xlog=True, xlabel='penalty'):
 
@@ -71,6 +160,7 @@ def plotAccuracyByPenalty(df, keyword, plot_accuracy=True, plot_nodeLeafCount=Fa
     plt.show()
 
 df = pd.read_csv('../results/grid_binary_breastcancer_data.libsvm.train_penF8.0_penT8.0_maxtrees100.0_maxdepth7.0_maxsize64000.0_precision1.0_logscaleTrue.csv')
-subset = df[(df['no_trees'] == 16) & (df['depth'] == 3)]
+subset = df[(df['no_trees'] == 16)]
 print(subset.describe())
-plotAccuracyByPenalty(subset, 'tinygbdt_penalty_split')
+#plot_grid(subset, 'tinygbdt_penalty_split')
+plotMetrics(subset)
