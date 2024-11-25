f883c325a069189ba45b00ece1a02a933057cec1
NinaHerrmann <nina.herrmann@uni-muenster.de>
2024-11-25 12:56:12 +0100

    minor compile changes

--- cbuild.sh
+++ cbuild.sh
@@ -5,14 +5,14 @@ cd experiments || exit
 
 ms=64000
 
-for dataset in "covtype"; do
+for dataset in "kin8nm" "california_housing"; do
   for i in $(seq -10 1 15); do
       for j in $(seq -10 1 15); do
-          for tree in 5 10 15 20 30 40 50 100 200 500 1000 5000 10000 100000; do
-              for depth in 3 5 7; do
+          for tree in 5 10 15 20 30 40 50 100 500; do
+              for depth in 3; do
                 fp=$(python3 -c "print(float(2**$i))" )
                 tp=$(python3 -c "print(float(2**$j))" )
-                if "../lightgbm" config=train.conf train_data=data/${dataset}.libsvm.binary.train valid_data=data/${dataset}.libsvm.binary.test config=train.conf max_depth=$depth num_trees=$tree tinygbdt_forestsize=$ms tinygbdt_penalty_split=$tp tinygbdt_penalty_feature=$fp output_model=Model/data-${dataset}ms-$ms-fp-$fp-tp-$tp-tree-${tree}-depth-${depth}.txt > Output/data-${dataset}ms-$ms-fp-$fp-tp-$tp-tree-${tree}-depth-${depth}.out; then
+                if "../lightgbm" config=train.conf objective=regression train_data=data/${dataset}.libsvm.train valid_data=data/${dataset}.libsvm.test config=train.conf max_depth=$depth num_trees=$tree tinygbdt_forestsize=$ms tinygbdt_penalty_split=$tp tinygbdt_penalty_feature=$fp output_model=Model/data-${dataset}ms-$ms-fp-$fp-tp-$tp-tree-${tree}-depth-${depth}.txt > Model/data-${dataset}ms-$ms-fp-$fp-tp-$tp-tree-${tree}-depth-${depth}.out; then
                     echo "Training model fp $fp tp $tp complete"
                 else
                     echo "Training model fp $fp tp $tp failed / not complete"
@@ -23,13 +23,10 @@ for dataset in "covtype"; do
   done
  done
 
-cd Model
+cd Model || exit
 
-find ./ -name 'data-covtypems-64000-fp-*' -type f -print0 | xargs -0 mv -t ../data/All/covtype/
-
-cd ../Output
-
-find ./ -name 'data-covtypems-64000-fp-*' -type f -print0 | xargs -0 mv -t ../data/All/covtype/
+find ./ -name 'data-kin8nmms-64000-fp-*' -type f -print0 | xargs -0 mv -t ../data/All/kin8nm/
+find ./ -name 'data-california_housingms-64000-fp-*' -type f -print0 | xargs -0 mv -t ../data/All/california_housing/
 
 cd ..
 
--- experiments/genData.sh
+++ experiments/genData.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-#cd ..
-#python3 -m venv path/to/venv
-#source path/to/venv/bin/activate
 cd python || exit
 
-for dataset in "/mushroom" "/kr-vs-kp" "/kin8nm" "/Breastcancer"; do
+for dataset in "/kin8nm" "/california_housing"; do
   python3 evaluate_models.py ${dataset}
 done
 
