1e2a0ac8ade2d623cc60b39b6c5abcb5332fdbd2
Nina Herrmann <ninaherrmann@D-3170L10.WIWI.UNI-MUENSTER.DE>
2024-11-29 16:01:25 +0100

    added plot script - adapted readme do the rest when data evaluation is finished

--- examples/buildModels.sh
+++ examples/buildModels.sh
@@ -21,10 +21,23 @@ ms=64000
 for csvfile in data/*.train.csv; do
   trainname="${trainfile#data/}"
   basename="${trainname%.*}"
-  for i in $(seq -10 1 15); do
-        for j in $(seq -10 1 15); do
-            for tree in 1 2 3 4 5 6 7 8 9 10 15 20 30 40 50 100 200 500 1000 10000; do
-                for depth in 3 5 7; do
+    for tree in 1 2 3 4 5 6 7 8 9 10 15 20 30 40 50 100 200 500 1000 10000; do
+        for depth in 3 5 7; do
+            if [[ trainname == *"kin8nm"* ]] || [[ trainname == *"california_housing"* ]]; then
+              if "../lightgbm" config=train.conf objective=regression train_data=data/trainname valid_data=data/${basename}.test.csv config=train.conf max_depth=$depth num_trees=$tree output_model=Model/data-${basename}-tree-${tree}-depth-${depth}.txt > Model/data-${dataset}-tree-${tree}-depth-${depth}.out; then
+                  echo "Training model fp $fp tp $tp complete"
+              else
+                  echo "Training model fp $fp tp $tp failed / not complete"
+              fi
+            else
+              if "../lightgbm" config=train.conf objective=binary train_data=data/trainname valid_data=data/${basename}.test.csv config=train.conf max_depth=$depth num_trees=$tree output_model=Model/data-${basename}-tree-${tree}-depth-${depth}.txt > Model/data-${dataset}-tree-${tree}-depth-${depth}.out; then
+                  echo "Training model fp $fp tp $tp complete"
+              else
+                  echo "Training model fp $fp tp $tp failed / not complete"
+              fi
+            fi
+            for i in $(seq -10 1 15); do
+                  for j in $(seq -10 1 15); do
                   fp=$(python3 -c "print(float(2**$i))" )
                   tp=$(python3 -c "print(float(2**$j))" )
                   if [[ trainname == *"kin8nm"* ]] || [[ trainname == *"california_housing"* ]]; then
@@ -48,10 +61,23 @@ done
 for trainfile in data/*.train; do
   trainname="${trainfile#data/}"
   basename="${trainfile%.*}"
-  for i in $(seq -10 1 15); do
-        for j in $(seq -10 1 15); do
-            for tree in 1 2 3 4 5 6 7 8 9 10 15 20 30 40 50 100 200 500 1000 10000; do
-                for depth in 3 5 7; do
+  for tree in 1 2 3 4 5 6 7 8 9 10 15 20 30 40 50 100 200 500 1000 10000; do
+      for depth in 3 5 7; do
+        if [[ trainname == *"kin8nm"* ]] || [[ trainname == *"california_housing"* ]]; then
+          if "../lightgbm" config=train.conf objective=regression train_data=data/trainname valid_data=data/${basename}.test.csv config=train.conf max_depth=$depth num_trees=$tree output_model=Model/data-${basename}-tree-${tree}-depth-${depth}.txt > Model/data-${dataset}-tree-${tree}-depth-${depth}.out; then
+              echo "Training model fp $fp tp $tp complete"
+          else
+              echo "Training model fp $fp tp $tp failed / not complete"
+          fi
+        else
+          if "../lightgbm" config=train.conf objective=binary train_data=data/trainname valid_data=data/${basename}.test.csv config=train.conf max_depth=$depth num_trees=$tree output_model=Model/data-${basename}-tree-${tree}-depth-${depth}.txt > Model/data-${dataset}-tree-${tree}-depth-${depth}.out; then
+              echo "Training model fp $fp tp $tp complete"
+          else
+              echo "Training model fp $fp tp $tp failed / not complete"
+          fi
+        fi
+          for i in $(seq -10 1 15); do
+                for j in $(seq -10 1 15); do
                   fp=$(python3 -c "print(float(2**$i))" )
                   tp=$(python3 -c "print(float(2**$j))" )
                   if [[ trainname == *"kin8nm"* ]] || [[ trainname == *"california_housing"* ]]; then
