4d18b8f04042196608bb4fec03eab644e89081e9
Jan Stenkamp <j_sten07@uni-muenster.de>
2025-01-16 15:08:53 +0100

    naive memory calculation

--- src/io/tree.cpp
+++ src/io/tree.cpp
@@ -364,7 +364,7 @@ std::string Tree::ToString() const {
   str_buf << "leaf_count="
     << ArrayToString(leaf_count_, num_leaves_) << '\n';
   str_buf << "model_size="
-    << (3 * 16 * (num_leaves_ -1) + 32 * (num_leaves_ - 1) + 32 * num_leaves_) << '\n'; // 16 bit left_child + 16 bit right_child + 16 bit split_feature + 32 bit leave_value + 32 bit threshold
+    << (3 * 32 * (num_leaves_ -1) + 32 * (num_leaves_ - 1) + 32 * num_leaves_) << '\n'; // 16 bit left_child + 16 bit right_child + 16 bit split_feature + 32 bit leave_value + 32 bit threshold
   str_buf << "internal_value="
     << ArrayToString(internal_value_, num_leaves_ - 1) << '\n';
   str_buf << "internal_weight="
