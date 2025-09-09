ccd3467d34ffe4e59bc8e41a94cfa0f8a271fcf2
jsten07 <j_sten07@uni-muenster.de>
2025-09-09 17:39:54 +0200

    debugging on palma

--- baselines/runBatchOfExperiments.sh
+++ baselines/runBatchOfExperiments.sh
@@ -5,5 +5,5 @@ data_dir="$2"
 result_dir="$3"
 
 while IFS=' ' read -r model dataset tree depth al; do
-    ./baslines/runSingleExperiment.sh "$data_dir" "$model" "$dataset" "$tree" "$depth" "$al" "$result_dir" 
+    ./baselines/runSingleExperiment.sh "$data_dir" "$model" "$dataset" "$tree" "$depth" "$al" "$result_dir" 
 done < "$chunkfile"
