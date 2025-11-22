2dab6071a7fa8efab65373ebb960dc04c125b6f8
Nina Herrmann <nina.herrmann@uni-muenster.de>
2025-11-22 13:56:41 +0100

    added seeds

--- experiments/python/get_dataset.py
+++ experiments/python/get_dataset.py
@@ -8,31 +8,33 @@ from ucimlrepo import fetch_ucirepo
 from sklearn.preprocessing import LabelEncoder
 import pandas as pd
 
-def fetch_data(data, target, name, flatten=False):
-    X_trainpre, X_val, y_trainpre, y_val = train_test_split(data, target, test_size=0.2, random_state=42)
+def fetch_data(data, target, name, flatten=False, seed=12):
+    X_trainpre, X_val, y_trainpre, y_val = train_test_split(data, target, test_size=0.2, random_state=seed)
     kf = KFold(n_splits=5)
     kf.get_n_splits(X_trainpre)
     if isinstance(X_trainpre, pd.DataFrame):
         X_trainpre = X_trainpre.to_numpy()
         y_trainpre = y_trainpre.to_numpy()
     for i, (train_index, test_index) in enumerate(kf.split(X_trainpre)):
-        if not os.path.exists(directory + '/' + str(i)):
-            os.makedirs(directory + '/' + str(i))
+        finaldir = directory+ '/' + f'{seed}/' + str(i)
+        seeddir = directory+ '/' + f'{seed}/'
+        if not os.path.exists(finaldir):
+            os.makedirs(finaldir)
         if flatten:
-            datasets.dump_svmlight_file(X_trainpre[train_index,:], y_trainpre[train_index].flatten(), os.path.join(directory + '/' + str(i), name + '.train'),
+            datasets.dump_svmlight_file(X_trainpre[train_index,:], y_trainpre[train_index].flatten(), os.path.join(finaldir, name + '.train'),
                                         zero_based=False)
-            datasets.dump_svmlight_file(X_trainpre[test_index,:], y_trainpre[test_index].flatten(), os.path.join(directory+ '/' + str(i), name + '.test'),
+            datasets.dump_svmlight_file(X_trainpre[test_index,:], y_trainpre[test_index].flatten(), os.path.join(finaldir, name + '.test'),
                                         zero_based=False)
         else:
-            datasets.dump_svmlight_file(X_trainpre[train_index,:], y_trainpre[train_index], os.path.join(directory+ '/' + str(i), name + '.train'), zero_based=False)
-            datasets.dump_svmlight_file(X_trainpre[test_index,:], y_trainpre[test_index], os.path.join(directory+ '/' + str(i), name + '.test'), zero_based=False)
+            datasets.dump_svmlight_file(X_trainpre[train_index,:], y_trainpre[train_index], os.path.join(finaldir, name + '.train'), zero_based=False)
+            datasets.dump_svmlight_file(X_trainpre[test_index,:], y_trainpre[test_index], os.path.join(finaldir, name + '.test'), zero_based=False)
 
     if flatten:
-        datasets.dump_svmlight_file(X_trainpre, y_trainpre.flatten(), os.path.join(directory, name + '.train'), zero_based=False)
-        datasets.dump_svmlight_file(X_val, y_val.flatten(), os.path.join(directory, name + '.val'), zero_based=False)
+        datasets.dump_svmlight_file(X_trainpre, y_trainpre.flatten(), os.path.join(seeddir, name + '.train'), zero_based=False)
+        datasets.dump_svmlight_file(X_val, y_val.flatten(), os.path.join(seeddir, name + '.val'), zero_based=False)
     else:
-        datasets.dump_svmlight_file(X_trainpre, y_trainpre, os.path.join(directory, name + '.train'), zero_based=False)
-        datasets.dump_svmlight_file(X_val, y_val, os.path.join(directory, name + '.val'), zero_based=False)
+        datasets.dump_svmlight_file(X_trainpre, y_trainpre, os.path.join(seeddir, name + '.train'), zero_based=False)
+        datasets.dump_svmlight_file(X_val, y_val, os.path.join(seeddir, name + '.val'), zero_based=False)
 
 # main function:
 if __name__ == "__main__":
@@ -44,90 +46,92 @@ if __name__ == "__main__":
 
     if not os.path.exists(directory):
         os.makedirs(directory)
+    seeds = [3, 4, 10, 11, 12]
+    for seed in seeds:
 
-    ########################
-    # Covtype dataset https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary.html
-    # if not os.path.exists(directory + '/covtype.test'):
-    #     path = wget.download('https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/covtype.libsvm.binary.bz2', os.path.join(directory, 'covtype.libsvm.binary.bz2'))
-    #
-    #     with bz2.open(os.path.join(directory, 'covtype.libsvm.binary.bz2')) as f:
-    #         # Decompress data from file
-    #         content = f.read()
-    #         # Write decompressed data to file
-    #         with open(os.path.join(directory, 'covtype.libsvm.binary'), 'wb') as f:
-    #             f.write(content)
-    #
-    #     covtype_libsvm = datasets.load_svmlight_file(os.path.join(directory, 'covtype.libsvm.binary'))
-    #     # convert target to [0,1] from [1,2]
-    #     ids = np.where(covtype_libsvm[1] == 1)
-    #     covtype_libsvm[1][ids]=0
-    #     ids = np.where(covtype_libsvm[1] == 2)
-    #     covtype_libsvm[1][ids]=1
-    #
-    #     fetch_data(covtype_libsvm[0], covtype_libsvm[1], 'covtype')
-    #     os.remove(os.path.join(directory, 'covtype.libsvm.binary.bz2'))
-    #     os.remove(os.path.join(directory, 'covtype.libsvm.binary'))
+        # ########################
+        # #Covtype dataset https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary.html
+        # if not os.path.exists(directory + '/covtype.test'):
+        #     path = wget.download('https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/covtype.libsvm.binary.bz2', os.path.join(directory, 'covtype.libsvm.binary.bz2'))
+        #
+        #     with bz2.open(os.path.join(directory, 'covtype.libsvm.binary.bz2')) as f:
+        #         # Decompress data from file
+        #         content = f.read()
+        #         # Write decompressed data to file
+        #         with open(os.path.join(directory, 'covtype.libsvm.binary'), 'wb') as f:
+        #             f.write(content)
+        #
+        #     covtype_libsvm = datasets.load_svmlight_file(os.path.join(directory, 'covtype.libsvm.binary'))
+        #     # convert target to [0,1] from [1,2]
+        #     ids = np.where(covtype_libsvm[1] == 1)
+        #     covtype_libsvm[1][ids]=0
+        #     ids = np.where(covtype_libsvm[1] == 2)
+        #     covtype_libsvm[1][ids]=1
+        #
+        #     fetch_data(covtype_libsvm[0], covtype_libsvm[1], 'covtype', seed=seed)
+        #     os.remove(os.path.join(directory, 'covtype.libsvm.binary.bz2'))
+        #     os.remove(os.path.join(directory, 'covtype.libsvm.binary'))
 
-    ########################
-    # # California housing dataset https://scikit-learn.org/dev/modules/generated/sklearn.datasets.fetch_california_housing.html
-    # if not os.path.exists(directory + '/california_housing.test'):
-    #     housing = datasets.fetch_california_housing()
-    #     fetch_data(housing.data, housing.target, 'california_housing')
-    #
-    # ########################
-    # # kin8nm dataset https://k8sapi.openml.org/d/189
-    # if not os.path.exists(directory + 'kin8nm.test'):
-    #     kin8nm = datasets.fetch_openml(data_id=189)
-    #     fetch_data(kin8nm.data, kin8nm.target, 'kin8nm')
-    #
-    # ########################
-    # # Mushroom dataset https://archive.ics.uci.edu/dataset/73/mushroom
-    # if not os.path.exists(directory + '/mushroom.test'):
-    #     mushroom = fetch_ucirepo(id=73)
-    #
-    #     X = mushroom.data.features
-    #     y = mushroom.data.targets
-    #     X = X.apply(LabelEncoder().fit_transform).to_numpy()
-    #     y = y.apply(LabelEncoder().fit_transform).to_numpy()
-    #     fetch_data(X, y, 'mushroom', True)
+        ########################
+        # California housing dataset https://scikit-learn.org/dev/modules/generated/sklearn.datasets.fetch_california_housing.html
+        # if not os.path.exists(directory + '/california_housing.test'):
+        #     housing = datasets.fetch_california_housing()
+        #     fetch_data(housing.data, housing.target, 'california_housing', seed=seed)
+        #
+        # ########################
+        # # kin8nm dataset https://k8sapi.openml.org/d/189
+        # if not os.path.exists(directory + 'kin8nm.test'):
+        #     kin8nm = datasets.fetch_openml(data_id=189)
+        #     fetch_data(kin8nm.data, kin8nm.target, 'kin8nm', seed=seed)
+        #
+        # ########################
+        # # Mushroom dataset https://archive.ics.uci.edu/dataset/73/mushroom
+        # if not os.path.exists(directory + '/mushroom.test'):
+        #     mushroom = fetch_ucirepo(id=73)
+        #
+        #     X = mushroom.data.features
+        #     y = mushroom.data.targets
+        #     X = X.apply(LabelEncoder().fit_transform).to_numpy()
+        #     y = y.apply(LabelEncoder().fit_transform).to_numpy()
+        #     fetch_data(X, y, 'mushroom', True, seed=seed)
 
-    ########################
-    # chess kr-vs-kp dataset https://archive.ics.uci.edu/dataset/22/chess+king+rook+vs+king+pawn
-    if not os.path.exists(directory + '/kr-vs-kp.test'):
-        chess = fetch_ucirepo(id=22)
+        ########################
+        # chess kr-vs-kp dataset https://archive.ics.uci.edu/dataset/22/chess+king+rook+vs+king+pawn
+        if not os.path.exists(directory + '/kr-vs-kp.test'):
+            chess = fetch_ucirepo(id=22)
 
-        X = chess.data.features
-        y = chess.data.targets
-        X = X.apply(LabelEncoder().fit_transform).to_numpy()
-        y = y.apply(LabelEncoder().fit_transform).to_numpy()
-        fetch_data(X, y, 'kr-vs-kp', True)
+            X = chess.data.features
+            y = chess.data.targets
+            X = X.apply(LabelEncoder().fit_transform).to_numpy()
+            y = y.apply(LabelEncoder().fit_transform).to_numpy()
+            fetch_data(X, y, 'kr-vs-kp', True, seed=seed)
 
-    ########################
-    # breastcancer wisconsin (diagnostic) https://archive.ics.uci.edu/dataset/17/breast+cancer+wisconsin+diagnostic
-    if not os.path.exists(directory + '/breastcancer.test'):
-        breastcancer = fetch_ucirepo(id=17)
-        X = breastcancer.data.features
-        y = breastcancer.data.targets
-        X = X.to_numpy()
-        y = y.apply(LabelEncoder().fit_transform).to_numpy()
-        fetch_data(X, y, 'breastcancer', True)
+        ########################
+        # breastcancer wisconsin (diagnostic) https://archive.ics.uci.edu/dataset/17/breast+cancer+wisconsin+diagnostic
+        if not os.path.exists(directory + '/breastcancer.test'):
+            breastcancer = fetch_ucirepo(id=17)
+            X = breastcancer.data.features
+            y = breastcancer.data.targets
+            X = X.to_numpy()
+            y = y.apply(LabelEncoder().fit_transform).to_numpy()
+            fetch_data(X, y, 'breastcancer', True, seed=seed)
 
-# ########################
-# # wine quality https://archive.ics.uci.edu/dataset/186/wine+quality
-# if not os.path.exists(directory + '/wine.test'):
-#     wine = fetch_ucirepo(id=186)
-#
-#     X = wine.data.features
-#     y = wine.data.targets
-#     X = X.to_numpy()
-#     y = y.apply(LabelEncoder().fit_transform).to_numpy()
-#     fetch_data(X, y, 'wine', True)
-#
-# if not os.path.exists(directory + '/covtype_multi.test'):
-#     covtype = fetch_ucirepo(id=31)
-#
-#     X = covtype.data.features
-#     y = covtype.data.targets
-#     X = X.to_numpy()
-#     y = y.apply(LabelEncoder().fit_transform).to_numpy()
-#     fetch_data(X, y, 'covtype_multi', True)
+        # # ########################
+        # # wine quality https://archive.ics.uci.edu/dataset/186/wine+quality
+        # if not os.path.exists(directory + '/wine.test'):
+        #     wine = fetch_ucirepo(id=186)
+        #
+        #     X = wine.data.features
+        #     y = wine.data.targets
+        #     X = X.to_numpy()
+        #     y = y.apply(LabelEncoder().fit_transform).to_numpy()
+        #     fetch_data(X, y, 'wine', True, seed=seed)
+        #
+        # if not os.path.exists(directory + '/covtype_multi.test'):
+        #     covtype = fetch_ucirepo(id=31)
+        #
+        #     X = covtype.data.features
+        #     y = covtype.data.targets
+        #     X = X.to_numpy()
+        #     y = y.apply(LabelEncoder().fit_transform).to_numpy()
+        #     fetch_data(X, y, 'covtype_multi', True, seed=seed)
--- hpc/executekFolds.py
+++ hpc/executekFolds.py
@@ -22,48 +22,50 @@ if __name__ == "__main__":
     parser.add_argument('--outdir', type=str,help='Directory to save datasets')
     parser.add_argument('--resdir', type=str, help='Directory to save datasets')
     args = parser.parse_args()
-    for fold in [0,1,2,3,4]:
-        subprocess.call(f"mkdir -p {args.output_model}", shell=True)
-        subprocess.call(f"./{args.lightgbm} config={args.config} \
-            objective={args.objective} \
-            num_class={args.num_class} \
-            metric={args.metric} \
-            train_data={args.data_dir}/{fold}/{args.dataset}.train \
-            valid_data={args.data_dir}/{fold}/{args.dataset}.test \
-            max_depth={args.max_depth} \
-            num_trees={args.num_trees} \
-            tinygbdt_forestsize={args.tinygbdt_forestsize} \
-            tinygbdt_penalty_split={args.tinygbdt_penalty_split} \
-            tinygbdt_penalty_feature={args.tinygbdt_penalty_feature} \
-            output_model={args.output_model}/{fold}.txt \
-                > {args.modeldir}/{fold}.out", shell=True)
-        subprocess.call(f"mkdir -p {args.outdir}/{args.resdir}", shell=True)
-        print(f"{args.outdir}/{args.resdir}/kfold.csv")
-        subprocess.call(f"python /home/n/n_herr03/toadkfolds/hpc/evaluation/evaluate_models.py \
-         --filename {args.modeldir}/{fold} --resultfile {args.outdir}/{args.resdir}/kfold.csv --test", shell=True)
-    kfoldresults = pd.read_csv(args.outdir + '/' + args.resdir + "/kfold.csv")
-    meanval = kfoldresults["accuracy"].mean()
-    print(meanval)
-    subprocess.call(f"./{args.lightgbm} config={args.config} \
+    seeds = [3, 4, 10, 11, 12]
+    for seed in seeds:
+        for fold in [0,1,2,3,4]:
+            subprocess.call(f"mkdir -p {args.output_model}", shell=True)
+            subprocess.call(f"./{args.lightgbm} config={args.config} \
                 objective={args.objective} \
                 num_class={args.num_class} \
                 metric={args.metric} \
-                train_data={args.data_dir}/{args.dataset}.train \
+                train_data={args.data_dir}/{seed}/{fold}/{args.dataset}.train \
+                valid_data={args.data_dir}/{seed}/{fold}/{args.dataset}.test \
                 max_depth={args.max_depth} \
                 num_trees={args.num_trees} \
                 tinygbdt_forestsize={args.tinygbdt_forestsize} \
                 tinygbdt_penalty_split={args.tinygbdt_penalty_split} \
                 tinygbdt_penalty_feature={args.tinygbdt_penalty_feature} \
-                output_model={args.output_model}.txt \
-                > {args.modeldir}.out", shell=True)
-    resultfile = f"{args.outdir}/results.csv"
-    subprocess.call(f"python /home/n/n_herr03/toadkfolds/hpc/evaluation/evaluate_models.py \
-            --filename {args.modeldir} --resultfile {args.outdir}/results.csv --val --mean={meanval}", shell=True)
-    subprocess.call(f"rm -rf {args.modeldir}", shell=True)
-    subprocess.call(f"rm -rf {args.modeldir}", shell=True)
-    subprocess.call(f"rm -rf {args.modeldir}.txt", shell=True)
-    subprocess.call(f"rm -rf {args.modeldir}.out", shell=True)
-    subprocess.call(f"rm -rf {args.outdir}/{args.resdir}", shell=True)
+                output_model={args.output_model}/{seed}/{fold}.txt \
+                    > {args.modeldir}/{seed}/{fold}.out", shell=True)
+            subprocess.call(f"mkdir -p {args.outdir}/{args.resdir}/{seed}", shell=True)
+            print(f"{args.outdir}/{args.resdir}/{seed}/kfold.csv")
+            subprocess.call(f"python /home/n/n_herr03/toadkfolds/hpc/evaluation/evaluate_models.py \
+             --filename {args.modeldir}/{seed}/{fold} --resultfile {args.outdir}/{args.resdir}/{seed}/kfold.csv --test", shell=True)
+        kfoldresults = pd.read_csv(f"{args.outdir}/{args.resdir}/{seed}/kfold.csv")
+        meanval = kfoldresults["accuracy"].mean()
+        print(meanval)
+        subprocess.call(f"./{args.lightgbm} config={args.config} \
+                    objective={args.objective} \
+                    num_class={args.num_class} \
+                    metric={args.metric} \
+                    train_data={args.data_dir}/{seed}/{args.dataset}.train \
+                    max_depth={args.max_depth} \
+                    num_trees={args.num_trees} \
+                    tinygbdt_forestsize={args.tinygbdt_forestsize} \
+                    tinygbdt_penalty_split={args.tinygbdt_penalty_split} \
+                    tinygbdt_penalty_feature={args.tinygbdt_penalty_feature} \
+                    output_model={args.output_model}/{seed}/{args.resdir}.txt \
+                    > {args.modeldir}/{seed}/{args.resdir}.out", shell=True)
+        resultfile = f"{args.outdir}/results.csv"
+        subprocess.call(f"python /home/n/n_herr03/toadkfolds/hpc/evaluation/evaluate_models.py \
+                --filename {args.modeldir}/{seed}/{args.resdir} --resultfile {args.outdir}/{seed}/results.csv --val --mean={meanval}", shell=True)
+        subprocess.call(f"rm -rf {args.modeldir}", shell=True)
+        subprocess.call(f"rm -rf {args.modeldir}", shell=True)
+        subprocess.call(f"rm -rf {args.modeldir}.txt", shell=True)
+        subprocess.call(f"rm -rf {args.modeldir}.out", shell=True)
+        subprocess.call(f"rm -rf {args.outdir}/{args.resdir}", shell=True)
 
 
 
