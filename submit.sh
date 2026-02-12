212c147668ae95020a7b7f7f0910c91b4d714569
Nina Herrmann <nina.herrmann@uni-muenster.de>
2026-02-12 11:01:34 +0100

    added generic submit file

---
+++ submit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+randomseeds=(1 2 5 6 7)
+datasets=("covtype" "covtype_multi" "california_housing" "kin8nm")
+for rs in "${randomseeds[@]}"; do
+  for dataset in "${datasets[@]}"; do
+    sbatch hpc/zen4_generic.sh "$rs" "$dataset"
+  done
+done
